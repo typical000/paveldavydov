1972a5299ec8283925757d0a52bdde4b370963fc
Pavel Davydov <typical000@gmail.com>
2017-04-10 00:15:50 +0300

    Add DOM helpers utils

---
+++ src/utils/dom.js
@@ -0,0 +1,20 @@
+import TWEEN from 'tween.js'
+
+/**
+ * Scroll down by one screen
+ * @param {Object} DOM element
+ */
+export const scrollScreen = (container) => {
+  const height = container.clientHeight
+  new TWEEN.Tween({x: container.scrollTop})
+    .to({x: height}, height * 4)
+    .easing(TWEEN.Easing.Exponential.Out)
+    .onUpdate(function() {
+      container.scrollTop = this.x
+    })
+    .start()
+}
+
+export default {
+  scrollScreen
+}
