7490b06345cfbd68e84180528e106575f6d7b2ed
Pavel Davydov <typical000@gmail.com>
2018-01-28 10:37:03 +0200

    Separate bulk data from contacts to module

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,47 +1,13 @@
 import React, {Component, createElement} from 'react'
 import withSizes from 'react-sizes'
 import PropTypes from 'prop-types'
+import data from './data'
 import AnimatedDisplayer from '../AnimatedDisplayer'
 import injectSheet from '../../utils/jss'
 import {xs as screenXs} from '../../constants/sizes'
 import {mediaSm, mediaXs} from '../../constants/media'
 import Container from '../Container'
 import Row from '../Row'
-import {Skype, Email, Twitter, Github, Linkedin} from '../icons'
-
-const data = {
-  skype: {
-    label: 'Skype',
-    text: 'typical000',
-    href: 'skype:typical000?chat',
-    component: Skype,
-  },
-  email: {
-    name: 'email',
-    label: 'Email',
-    text: 'typical000@gmail.com',
-    href: 'mailto:typical000@gmail.com',
-    component: Email,
-  },
-  twitter: {
-    label: 'Twitter',
-    text: '@typical001',
-    href: 'https://twitter.com/typical001',
-    component: Twitter,
-  },
-  github: {
-    label: 'GitHub',
-    text: 'typical000',
-    href: 'https://github.com/typical000',
-    component: Github,
-  },
-  linkedin: {
-    label: 'Linkedin',
-    text: 'PavelDavydov',
-    href: 'https://www.linkedin.com/in/pavel-davydov-09892aa5/',
-    component: Linkedin,
-  }
-}
 
 const styles = theme => ({
   contact: {
